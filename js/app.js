f2720303a0f0ba1fb885423446301630186d495e
Marcelino Pérez <32309834+mperez01@users.noreply.github.com>
2017-11-20 18:36:08 +0100

    ejemplo escuchando en puerto 3000

--- js/app.js
+++ js/app.js
@@ -1,27 +1,37 @@
 "use strict";
 
 const mysql = require("mysql");
-const http = require("http");
-const express = require("express");
 const config = require("./config");
 const daoUsers = require("./dao_users");
+const express = require("express");
 
+//----------------------
+const app = express();
+//----------------------
 
-const pool= mysql.createPool({
+const pool = mysql.createPool({
     host: config.host,
     user: config.user,
     password: config.password,
     database: config.database
 });
 
-let daoUser = new daoUsers.DAOUsers(pool);
-daoUser.isUserCorrect("user", "pass", (err, result) => {
+app.get("/", (request, response) => {
+    response.status(200);
+    response.type("text/plain; charset=utf-8"); 
+    response.end("Directorio raiz");
+});
+
+app.get("/index.html", (request, response) => {
+    response.status(200);
+    response.type("text/plain; charset=utf-8"); 
+    response.end("Aqui se muestra index.html");
+});
+
+app.listen(3000, (err) => {
     if (err) {
-        console.error(err);
-    } else if (result) {
-        console.log("Usuario y contraseña correctos");
+        console.error("No se pudo inicializar el servidor: " + err.message);
     } else {
-        console.log("Usuario y/o contraseña incorrectos");
+        console.log("Servidor arrancado en el puerto 3000");
     }
-    pool.end();
 });
