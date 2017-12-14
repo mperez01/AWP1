a8f83b6f60e4590e2e9c3b757ca4335cd49898e6
Marcelino Pérez <32309834+mperez01@users.noreply.github.com>
2017-12-14 04:03:23 +0100

    database update

--- DB/facebluff.sql
+++ DB/facebluff.sql
@@ -2,10 +2,10 @@
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 13-12-2017 a las 13:06:19
+-- Servidor: localhost
+-- Tiempo de generación: 14-12-2017 a las 04:02:36
 -- Versión del servidor: 10.1.28-MariaDB
--- Versión de PHP: 7.1.10
+-- Versión de PHP: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,17 +34,6 @@ CREATE TABLE `answer` (
   `text` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `answer`
---
-
-INSERT INTO `answer` (`id`, `id_question`, `text`) VALUES
-(55, 15, '1\r'),
-(56, 15, '2\r'),
-(57, 15, '3\r'),
-(58, 15, '4'),
-(59, 15, 'Holi');
-
 -- --------------------------------------------------------
 
 --
@@ -57,13 +46,6 @@ CREATE TABLE `questions` (
   `text` text NOT NULL,
   `num_answ` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `questions`
---
-
-INSERT INTO `questions` (`id`, `user_id`, `text`, `num_answ`) VALUES
-(15, 24, 'Pregunta', 4);
 
 -- --------------------------------------------------------
 
@@ -78,14 +60,6 @@ CREATE TABLE `relationship` (
   `action_user_id` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `relationship`
---
-
-INSERT INTO `relationship` (`user_one_id`, `user_two_id`, `status`, `action_user_id`) VALUES
-(1, 19, 1, 1),
-(1, 24, 0, 24);
-
 -- --------------------------------------------------------
 
 --
@@ -97,15 +71,6 @@ CREATE TABLE `sessions` (
   `expires` int(11) UNSIGNED NOT NULL,
   `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `sessions`
---
-
-INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-('JnT0dxBn7EeS2ZQqbXDLGpNIeFobbr77', 1513195966, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUserId\":24,\"userImg\":\"aa5b2da5c7ae5d70dd03837d0ee7cd46\"}'),
-('Sq1brhkPujkXEcs0Am9HhnxV3pO4HTTP', 1513174661, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUserId\":19,\"currentUserEmail\":\"mario@ucm.es\",\"userImg\":\"d7ec16cf43d6cb10e32bf673c29a2ad4\"}'),
-('ZHo94bXlUS-t_qRAOEKRnws6Zj5_rmgB', 1513249011, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUserId\":19,\"currentUserEmail\":\"mario@ucm.es\",\"userImg\":\"Capt Spaulding-01.png\"}');
 
 -- --------------------------------------------------------
 
@@ -124,17 +89,6 @@ CREATE TABLE `user` (
   `points` int(10) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `user`
---
-
-INSERT INTO `user` (`user_id`, `email`, `password`, `name`, `gender`, `dateOfBirth`, `image`, `points`) VALUES
-(1, 'marce93p@gmail.com', '1234', 'Marcelino Pérez', 'male', '1993-06-30', '35b63474abbd3a896e5acce19ccf83e0', 0),
-(19, 'mario@ucm.es', 'pass', 'Mario Rodríguez Salinero', 'male', '1994-01-14', 'Capt Spaulding-01.png', 0),
-(23, 'marzia@astolfi.com', '1234', 'Marzia Astolfi', 'female', '1994-06-04', 'd7ec16cf43d6cb10e32bf673c29a2ad4', 0),
-(24, 'jorge@gmail.com', '1234', 'Jose Luis', 'male', '2017-12-12', 'aa5b2da5c7ae5d70dd03837d0ee7cd46', 0),
-(25, 'sabela@ucm.es', 'pass', 'Sabela García', 'female', '1996-04-18', 'd507c264354c599eff93e421f3013431', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -146,14 +100,6 @@ CREATE TABLE `user_answer` (
   `id_user` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `user_answer`
---
-
-INSERT INTO `user_answer` (`id_answer`, `id_user`) VALUES
-(55, 25),
-(59, 19);
-
 -- --------------------------------------------------------
 
 --
@@ -161,9 +107,10 @@ INSERT INTO `user_answer` (`id_answer`, `id_user`) VALUES
 --
 
 CREATE TABLE `user_guess` (
-  `user_id_answer` int(10) UNSIGNED NOT NULL,
-  `user_id_guess` int(10) UNSIGNED NOT NULL,
-  `correct` tinyint(1) NOT NULL
+  `id_user` int(10) UNSIGNED DEFAULT NULL,
+  `id_friend` int(10) UNSIGNED DEFAULT NULL,
+  `id_answer` int(10) UNSIGNED DEFAULT NULL,
+  `correct` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -217,8 +164,9 @@ ALTER TABLE `user_answer`
 -- Indices de la tabla `user_guess`
 --
 ALTER TABLE `user_guess`
-  ADD KEY `user_id_answer` (`user_id_answer`),
-  ADD KEY `user_id_guess` (`user_id_guess`);
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_friend` (`id_friend`),
+  ADD KEY `id_answer` (`id_answer`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -228,19 +176,19 @@ ALTER TABLE `user_guess`
 -- AUTO_INCREMENT de la tabla `answer`
 --
 ALTER TABLE `answer`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT de la tabla `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Restricciones para tablas volcadas
@@ -277,8 +225,9 @@ ALTER TABLE `user_answer`
 -- Filtros para la tabla `user_guess`
 --
 ALTER TABLE `user_guess`
-  ADD CONSTRAINT `user_guess_ibfk_1` FOREIGN KEY (`user_id_answer`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_guess_ibfk_2` FOREIGN KEY (`user_id_guess`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `user_guess_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `user_guess_ibfk_2` FOREIGN KEY (`id_friend`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `user_guess_ibfk_3` FOREIGN KEY (`id_answer`) REFERENCES `answer` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
